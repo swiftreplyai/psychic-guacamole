5617b48a8c6fb538c46b132ee7f03941174d8678
swiftreplyai <127563901+swiftreplyai@users.noreply.github.com>
2023-03-11 12:17:07 -0500

    Update swiftfinal.py

--- swiftfinal.py
+++ swiftfinal.py
@@ -9,7 +9,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-local_css("styles.css")
+local_css("style.css")
 
 st.title("SwiftReply: Automatic Email Response Generation")
 st.sidebar.header("Instructions")
